60d8cadaf1bc98acb9f87738cd857e779d71bd9e
Hyejin00 <svivs137@naver.com>
2020-06-06 00:11:36 +0900

    Add MenuBtn in header - BottomTabNavgator

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -6,11 +6,42 @@ import SearchScreen from '../screens/SearchScreen';
 import NewsScreen from '../screens/NewsScreen';
 import ActivityScreen from '../screens/ActivityScreen';
 import Colors from '../constants/Colors';
+import { TouchableWithoutFeedback } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const BottomTab = createBottomTabNavigator();
 const INITIAL_ROUTE_NAME = 'Home';
 
+function getHeaderTitle(route) {
+  const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+
+  switch (routeName) {
+    case 'Home':
+      return 'Home';
+    case 'Search':
+      return 'Search';
+    case 'News':
+      return 'News';
+    case 'Activity':
+      return 'Activity';
+  }
+}
+
 export default function BottomTabNavigator({ navigation, route }){
+  navigation.setOptions({ 
+    headerTitle: getHeaderTitle(route),
+    headerLeft: ()=>(
+    <TouchableWithoutFeedback
+      onPress={()=>{navigation.openDrawer()}}>
+      <Ionicons
+      name= 'md-menu'
+      size={24}
+      color='black'
+      style={{marginLeft:20}}/>
+    </TouchableWithoutFeedback>
+    )
+  });
+
   return(
     <BottomTab.Navigator
       initialRouteName={INITIAL_ROUTE_NAME}
